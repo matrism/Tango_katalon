40b6d16d1fa38c16a6d65d796b35e9aeb7c01700
mmazurenko <maria.mazurenko@dataart.com>
2014-03-19 17:58:50 +0200

    Added protractor tests for provisioning first step

---
+++ protractor_tests/tests/pages/provisioning.js
@@ -0,0 +1,38 @@
+var //config = require("config"),
+    provisioning_page = {
+        
+        url: "http://security-console.devportal-ci.dspdev.wmg.com/provisioning",
+        
+        goToPage: function() {
+            browser.get(provisioning_page.url);
+        },
+        
+        elementByCssIsPresent: function(cssSelector, shouldBePresent) {
+            expect(element(by.css(cssSelector)).isPresent()).toBe(shouldBePresent);
+        },
+        
+        setValue: function(cssSelector, value) {
+            element(by.css(cssSelector)).sendKeys(value);
+        },
+        
+        selectItemFromRepeater: function(ngRepeaterName, itemIndex) {
+            element(by.repeater(ngRepeaterName).row(itemIndex)).click();
+        },
+        
+        repeaterHasItem: function(ngRepeaterName, itemContent) {
+            var all = element.all(by.repeater(ngRepeaterName));
+            
+            all.each(function(item) {
+                item.getText().then(function(text) {
+                    expect(text).toContain(itemContent);
+                });
+            });
+        },
+        
+        elementByCssIsDisplayed: function(cssSelector, shouldBeDisplayed) {
+            expect(element(by.css(cssSelector)).isDisplayed()).toBe(shouldBeDisplayed);
+        }
+        
+    };
+
+module.exports = provisioning_page;
