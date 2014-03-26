fb14557cac394606c43697b73a16a7a48bccb8be
mmazurenko <maria.mazurenko@dataart.com>
2014-03-26 16:48:34 +0200

    Added matchers factory

--- modules/matchers.js
+++ modules/matchers.js
@@ -1,27 +1,34 @@
-matchers = {
+var Matcher = function() {};
+
+Matcher.prototype.create = function(options) {
     
-    add: function(scope) {
-
-        scope.addMatchers({
-            
-            shouldContain: function(elName, content) {
-                var elValue = this.actual,
-                    pass = (elValue.indexOf(content) !== -1) ? true : false,
-                    isNot = (this.isNot) ? 'not ' : '';
-                
-                this.message = function() {
-                    return elName + ' should ' + isNot + 'contain ' + content;
-                };
-                
-                return pass;
-            }
-
-        });
-        
+    var type = options.type;
+    
+    // error if the matcher doesn't exist
+    if (typeof Matcher[type] !== 'function') {
+        throw {
+            name: 'Error',
+            message: 'Matcher ' + type + ' does not exist'
+        };
     }
+    
+    return new Matcher[type](options);
     
 };
 
-module.exports = matchers;
+Matcher.ShouldContain = function(options) {
+    this.compare = function(elName, content) {
+        var elValue = this.actual,
+            pass = (elValue.indexOf(content) !== -1) ? true : false,
+            isNot = (this.isNot) ? 'not ' : '';
 
+        this.message = function() {
+            //elName + ' should ' + isNot + 'contain ' + content;
+            return options.message;
+        };
 
+        return pass;
+    };
+};
+
+module.exports = Matcher;
