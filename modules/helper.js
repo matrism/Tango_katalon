f56cc5dc2a549af13541f3d31fca34d4e602165a
Mikrovolnovka <yaroshenkod@gmail.com>
2014-03-27 11:39:47 +0200

    new helper functions

--- modules/helper.js
+++ modules/helper.js
@@ -4,7 +4,96 @@ var Helper = {
         elem.getAttribute(attr_name).then(function(attr) {
             expect(attr).toContain(value);
         });
+    },
         
+    saveScreen: function(name) {
+        name = name || "screen";
+        browser.takeScreenshot().then(function(png) {
+            var fs = require('fs'),
+                date = new Date(),
+                newdate = date.getDate() + '_' + date.getMonth() + '_' + date.getFullYear() + "-" + date.getHours() + "_" + date.getMinutes() + "_" + date.getSeconds(),
+                filename = "screens/" + name + "_" + newdate + ".png",
+                stream = fs.createWriteStream(filename);
+
+            console.log("stream", stream)
+
+            stream.write(new Buffer(png, "base64"));
+            stream.end();
+        });
+    },
+    
+    clickOnOneEqual: function(elements, item) {
+        elements.each(function(elem) {
+            elem.getText().then(function(text) {
+                if (text === item) {
+                    elem.click();
+                }
+            });
+        });
+    },
+    
+    clickOnOneInclude: function(elements, item) {
+        elements.each(function(elem) {
+            elem.getText().then(function(text) {
+                if (text.indexOf(item) >= 0) {
+                    elem.click();
+                }
+            });
+        });
+    },
+    
+    clickOnSame: function(elements, item, time) {
+        var i = 0;
+        elements.each(function(elem) {
+            elem.getText().then(function(text) {
+                if (text.indexOf(item) >= 0) {
+                    i++;
+                }
+                if (i === time) {
+                    elem.click();
+                }
+            });
+        });
+    },
+    
+    returnOneOfMany: function(elements, item) {
+        var flag = false;
+        
+        elements.each(function(elem) {
+            elem.getText().then(function(text) {
+                if (text === item) {
+                    flag = true;
+                }
+            });
+        });
+        
+        browser.wait(function() {
+            return flag;
+        }, 1500);
+        
+        if (!flag) {
+            throw (new Error("Cannot find item " + item + " in the list"))
+        }
+    },
+    
+    splitAndCompare: function(text, elements) {
+        var ar = text.split("; "),
+            actual_array = [],
+            items = elements.length;
+    
+        elements.each(function(el) {
+            el.getText().then(function(text) {
+                actual_array.push(text);
+            });
+        });
+        
+        browser.wait(function() {
+            return elements.count().then(function(count){
+                return count === actual_array.length;
+            });
+        });
+        
+        expect(elements.count()).toEqual(actual_array.length);
     },
     /**
      * @name waitForUrlToChangeTo
@@ -26,6 +115,15 @@ var Helper = {
                 });
             }
         );
+    },
+    
+    waitForElement: function(el, timeout) {
+        timeout = timeout || 2000;
+        browser.wait(function() {
+            return el.isElementPresent().then(function(present) {
+                return present;
+            });
+        }, timeout);
     }
 };
 
