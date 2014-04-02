bcf3d29ddf9ea6634c2a9231e6d1a02b000a434c
Mikrovolnovka <yaroshenkod@gmail.com>
2014-04-02 12:44:22 +0300

    adding new structure with tags

--- modules/controller.js
+++ modules/controller.js
@@ -7,28 +7,36 @@ var _ = require("underscore"),
             if (!(array instanceof Array) || array.length < 1) {
                 throw (new Error("Incorrect features number"));
             }
-
+            if (!(_tf_config._cli.tags instanceof Array) && _.isString(_tf_config._cli.tags)) {
+                _tf_config._cli.tags = [_tf_config._cli.tags];
+            }
             for(var i in array) {
                 feature = require("../../../new_tests/features/"+array[i]);
-                controller.processFeature(feature);
+                if (feature instanceof Array) {
+                    controller.processArrayFeatures(feature);
+                } else {
+                    controller.processFeature(feature);
+                }
             }
         },
+        processArrayFeatures: function(features) {
+            for (var i in features) {
+                controller.processFeature(features[i]);
+            } 
+        },
         processFeature: function(feature) {
-            
             if (
                     typeof _tf_config._cli.tags === undefined
-                    ||
-                    !(_tf_config._cli.tags instanceof Array)
                     ||
                     _tf_config._cli.tags.length === 0
                     ||
                     (typeof feature.tags !== undefined
                     &&
-                    feature.tags.tags instanceof Array
+                    feature.tags instanceof Array
                     &&
-                    feature.tags > 0
+                    feature.tags.length > 0
                     &&
-                    _.intersection(_tf_config._cli.tags,feature.tags).length > 0)
+                    _.intersection(_tf_config._cli.tags, feature.tags).length > 0)
             ) {
                 describe(feature.name, function() {
                     if (typeof feature.beforeEach !== "undefined") {
