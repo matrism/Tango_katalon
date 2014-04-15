709a0af7de4821a4d043a11c019ecd486fa22643
Mikrovolnovka <yaroshenkod@gmail.com>
2014-04-15 16:49:18 +0300

    REST service helper

--- modules/services.js
+++ modules/services.js
@@ -1,4 +1,5 @@
 var http = require("request-sync"),
+    _ = require("underscore"),
     Services = {
         getAccessToken: function() {
             var options = {
@@ -23,6 +24,47 @@ var http = require("request-sync"),
                 throw new Error("Failed to obtain access_token for #{" + _tf_config.client_id + "}")
             }
             return "Bearer #{" + JSON.parse(res.body).access_token + "}";
+        },
+        tutorials_service_url: _tf_config.urls.devportal_service,
+        tutorialClient: function() {
+            return new DataServiceClient(Services.tutorials_service_url, Services.getAccessToken());
+        },
+        applications_service_url: _tf_config.urls.webconsole_service,
+        applicationClient: function() {
+            return new DataServiceClient(Services.applications_service_url, Services.getAccessToken());
+        },
+        parseResponseFields: function(res, field) {
+            var array = [], entity, i;
+                
+            for (i in res['data']) {
+                entity = res['data'][i];
+                array.push(entity[field]);
+            }
+            return array;
+        },
+        getAllFatesCategories: function(res) {
+            var array = [], terms, i;
+            
+            for (i in res['facets']) {
+                terms = res['facets'][i];
+                array.push(terms['terms']['name']);
+            }
+            return array;
+        },
+        getItemFieldNames: function(res) {
+            var array = [], items, i, j, field;
+            
+            for (i in res['data']) {
+                items = res['data'][i];
+                for (j in items) {
+                    field = items[j];
+                    array.push(field[0]);
+                }
+            }
+            return _.uniq(array);
+        },
+        getApplicationsFromResponse: function(res) {
+            return JSON.parse(res);
         }
     };
     
