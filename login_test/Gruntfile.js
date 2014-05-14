0c623bfa7521eab801edcbd9ba81860d32878d03
Mikrovolnovka <yaroshenkod@gmail.com>
2014-05-14 17:45:47 +0300

    http://jira.wmg.com/browse/DEVP-2698

---
+++ login_test/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+    'use strict';
+    grunt.loadTasks('./vendor/factory-testing-framework/modules/parallel');
+    
+    grunt.initConfig({
+        parallel: {
+            shell: {
+                tasks: [{
+                    cmd: "bash",
+                    args: ["start.sh", "--tags", "feature_file_tag", "--reporting", "all"]
+                },{
+                    cmd: "bash",
+                    args: ["start.sh", "--tags", "feature_file_tag_2", "--reporting", "all"]
+                }]
+            }
+        }
+    });
+    
+    grunt.registerTask('check', function() {
+        if (grunt.__failed) {
+            grunt.fail.warn("Done with errors");
+        }
+    })
+
+    grunt.registerTask('default', ['parallel','check']);
+};
