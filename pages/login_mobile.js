304c3d57aefe9cdfa9cdba422751fc178de15c1e
Mikrovolnovka <yaroshenkod@gmail.com>
2014-06-11 13:31:09 +0300

    added Mobile branch

---
+++ pages/login_mobile.js
@@ -0,0 +1,56 @@
+var PageObject = require("../modules/pageObject");
+    
+var loginPage = function(_tf_config, lg_element) {
+    return new PageObject({
+        locators: {
+            username: { css: "#username" },
+            password: { css: "#password" },
+            login_button: { css: "button[type='submit']" }
+        },
+        setUsername: function(username) {
+            pages.login.elems.username.clear();
+            pages.login.elems.username.sendKeys(username);
+            return this;
+        },
+        setPassword: function(password) {
+            pages.login.elems.password.clear();
+            pages.login.elems.password.sendKeys(password);
+            return this;
+        },
+        doLogin: function(login_name, login_pass) {
+            login_name = login_name || _tf_config.user_name;
+            login_pass = login_pass || _tf_config.user_password;
+            pages.login
+                .setUsername(login_name)
+                .setPassword(login_pass);
+
+            browser.wait(function() {
+                return pages.login.shouldWeLogin(login_name, login_pass);
+            });
+
+            pages.login.elems.login_button.click();
+            return this;
+        },
+        shouldWeLogin: function(login_name, login_pass) {
+            return pages.login.elems.password.getAttribute("value").then(function(text) {
+                return text === login_pass;
+            }) && pages.login.elems.username.getAttribute("value").then(function(text) {
+                return text === login_name;
+            });
+        },
+        login: function(login_name, login_pass) {
+            login_name = login_name || _tf_config.user_name;
+            login_pass = login_pass || _tf_config.user_password;
+            pages.login.open(true).waitForAjax();
+            pages.login.elems.username.isPresent().then(function(present) {
+                if (present) {
+                    pages.login.doLogin(login_name, login_pass);
+                } 
+            });
+            lg_element = lg_element || element(By.partialLinkText(login_name.toUpperCase()));
+            expect(lg_element.isPresent()).toBe(true);
+        }
+    });
+};
+
+module.exports = loginPage;
--- testingFramework.js
+++ testingFramework.js
@@ -8,7 +8,16 @@ global.ftf = ftf = {
     htmlReporter: require("./modules/htmlReporter"),
     services: require("./modules/services"),
     dataServicesClient: require("./modules/dataServicesClient"),
-    request: require("./modules/request")
+    request: require("./modules/request"),
+    mobile: {
+        configer: require("./modules/configer"),
+        pageObject: require("./modules/pageObject"),
+        matchers: require("./modules/matchers"),
+        helper: require("./modules/helper"),
+        loginPage: require("./pages/login_mobile"),
+        controller: require("./modules/controller"),
+        htmlReporter: require("./modules/htmlReporter")
+    }
 };
 
 module.exports = global.ftf;
