c878d9e7821bd7f1006f46cd1daa3bdb423fb166
Felipe Klein Zigmundo <felipe.zigmundo@wmg.com>
2014-09-09 11:57:00 -0300

    Add few more tests for sub-publisher org section

--- pages/createOrganisation.js
+++ pages/createOrganisation.js
@@ -1,13 +1,22 @@
+'use strict';
+
 function CreateOrganisation () {
     this.subPublisher = {
         yesButton: element.all(by.model('orgHas.subPublishers')).get(0),
-        noButton: element.all(by.model('orgHas.subPublishers')).get(1)
+        noButton: element.all(by.model('orgHas.subPublishers')).get(1),
+        section: element(by.css('div[data-ng-show="orgHas.subPublishers"]')),
+        repeater: element.all(by.repeater('sp in getSubPublishers()')),
+        addSubPublisherButton: element(by.css('button[data-ng-click="addSubPublisher()"]'))
     };
-};
+}
 
 CreateOrganisation.prototype = {
     get: function () {
         browser.get('/create/org');
+    },
+
+    getSubPublisherByIndex: function (index) {
+        return this.subPublisher.repeater.get(index);
     },
 
     toggleHasSubPublisherOption: function (option) {
@@ -19,6 +28,29 @@ CreateOrganisation.prototype = {
             this.subPublisher.noButton.click();
         }
 
+    },
+
+    addSubPublisher: function () {
+        browser.actions().mouseMove(this.subPublisher.addSubPublisherButton).perform();
+        this.subPublisher.addSubPublisherButton.click();
+    },
+
+    removeSubPublisher: function (index, confirmation) {
+        var byCssRemoveButton = by.css('button[data-ng-click="showDeleteSubPublisherModal($index)"]'),
+            button = this.getSubPublisherByIndex(index).element(byCssRemoveButton),
+            confirmationButton;
+
+        browser.actions().mouseMove(button).perform();
+        button.click();
+
+        if (confirmation) {
+            confirmationButton = element(by.css('.modal-footer > button[data-ng-click="ok()"]'));
+        } else {
+            confirmationButton = element(by.css('.modal-footer > button[data-ng-click="cancel()"]'));
+        }
+
+        browser.actions().mouseMove(confirmationButton).perform();
+        confirmationButton.click();
     }
 
 };
--- specs/org/createOrganisationSpec.js
+++ specs/org/createOrganisationSpec.js
@@ -13,7 +13,30 @@ describe(' Create Organisation E2E ::', function () {
 
         it(' Should toggle between yes and no', function () {
             page.toggleHasSubPublisherOption(true);
+            expect(page.subPublisher.section.isDisplayed()).toBeTruthy();
+
             page.toggleHasSubPublisherOption(false);
+            expect(page.subPublisher.section.isDisplayed()).toBeFalsy();
+        });
+
+        it(' Should add and remove - when available - the sub-publisher item', function () {
+            var byCssRemoveButton = by.css('button[data-ng-click="showDeleteSubPublisherModal($index)"]');
+
+            page.toggleHasSubPublisherOption(true);
+            expect(page.subPublisher.repeater.count()).toBe(1);
+
+            expect(page.getSubPublisherByIndex(0).element(byCssRemoveButton).isDisplayed()).toBeFalsy();
+
+            page.addSubPublisher();
+            expect(page.subPublisher.repeater.count()).toBe(2);
+            expect(page.getSubPublisherByIndex(0).element(byCssRemoveButton).isDisplayed()).toBeFalsy();
+            expect(page.getSubPublisherByIndex(1).element(byCssRemoveButton).isDisplayed()).toBeTruthy();
+
+            page.removeSubPublisher(1, false);
+            expect(page.subPublisher.repeater.count()).toBe(2);
+
+            page.removeSubPublisher(1, true);
+            expect(page.subPublisher.repeater.count()).toBe(1);
         });
     });
 });
