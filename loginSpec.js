99b9dcb0543b42c069155c2dfb6a953b80a8b07e
Eric Colman <eric.colman@wmg.com>
2014-09-09 11:52:50 -0700

    Updated tests

--- loginSpec.js
+++ loginSpec.js
@@ -1,7 +1,7 @@
 // logs into the tango application
 // should be first spec to be run, as it logs in for all following tests
 describe("Tango :: Login", function() {
-    it("should login to SSO", function() {
+    iit("should login to SSO", function() {
         browser.ignoreSynchronization = true;
         browser.manage().timeouts().implicitlyWait(10000);
 
