ea7027bcd72009d600efd8996fe175a4e4d69288
Eric Colman <ecolman@axispoint.com>
2014-09-15 15:13:10 -0400

    Updated tests

---
+++ specs/deal/createDealSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe("Create Deal :: ", function () {
+    beforeEach(function () {
+        browser.get('/#/create/deal');
+    });
+
+    describe("General Section :: ", function() {
+        it("should select a contracting party", function () {
+            
+        });
+    });
+});
