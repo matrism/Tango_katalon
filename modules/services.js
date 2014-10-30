b5d663aff7f901a93937cdfbd57b75fa09d2d8d8
Mikrovolnovka <yaroshenkod@gmail.com>
2014-10-30 12:50:54 +0200

    del request with body ???

--- modules/services.js
+++ modules/services.js
@@ -2,9 +2,9 @@ var http = require("./request"),
     _ = require("underscore"),
     DataServiceClient = require("./dataServicesClient"),
     Services = {
-        restClient: function(endpoint, use_cookies) {
+        restClient: function(endpoint, use_cookies, debug) {
             use_cookies = use_cookies || false;
-            global.restClient = new DataServiceClient(endpoint, use_cookies);
+            global.restClient = new DataServiceClient(endpoint, use_cookies, debug);
             return global.restClient;
         },
         parseResponseFields: function(res, field) {
