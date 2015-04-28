6034014e47c95377078b309607216af2d23f5057
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-04-28 18:38:00 -0300

    Implement random.date helper.

--- helpers/random.js
+++ helpers/random.js
@@ -1,4 +1,15 @@
 "use strict";
+var _ = require("lodash");
 exports.id = function() {
 	return Date.now().toString() + Math.floor(Math.random() * 1000);
+};
+exports.date = function(a, b) {
+	a = a.getTime();
+	if(b) {
+		b = b.getTime();
+	}
+	else {
+		b = Date.now();
+	}
+	return new Date(_.random(a, b));
 };
