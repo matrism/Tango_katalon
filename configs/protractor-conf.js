7f936209ca86710dcd63f461a2b2de819d8824e5
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-05-13 12:21:37 -0300

    Allow config file to specify a Selenium address to connect.
    When specified, start.sh won't run its own instance of WebDriver.
    It should be previously running (via `webdriver-manager start`).

--- configs/protractor-conf.js
+++ configs/protractor-conf.js
@@ -3,7 +3,7 @@ var path = require("path"),
     ScreenShotReporter, config, SSReporter_instance;
 
 global.ftf = require("../vendor/factory-testing-framework");
-global._tf_config = require("./config"); 
+global._tf_config = require("./config");
 ScreenShotReporter = ftf.htmlReporter;
 SSReporter_instance = new ScreenShotReporter({baseDirectory: "reports/html"});
 
@@ -14,8 +14,6 @@ global.hash = {};
 require("../helpers/services_helper");
 
 config = {
-    chromeOnly: true,
-    chromeDriver: '../node_modules/protractor/selenium/chromedriver',
     capabilities: {
         "browserName": _tf_config._system_.browser, //firefox, ie
         'chromeOptions': {
@@ -28,10 +26,10 @@ config = {
         var reporting = _tf_config._system_.reporting;
         var matchers;
         browser.driver.manage().timeouts().setScriptTimeout(15000);
-    
+
         if (_tf_config._system_.resolution.width && _tf_config._system_.resolution.height)
             browser.driver.manage().window().setSize(_tf_config._system_.resolution.width, _tf_config._system_.resolution.height);
-    
+
         if (reporting === "xml" || reporting === "all") {
             require("jasmine-reporters");
             jasmine.getEnv().addReporter(
@@ -42,7 +40,7 @@ config = {
         if (reporting === "html" || reporting === "all") {
             jasmine.getEnv().addReporter(SSReporter_instance);
         }
-        
+
         if (typeof process.env.__using_grunt === "undefined") {
             var spawn = require('child_process').spawn
             var child = spawn("bash", ["grunt","clearReports"]);
@@ -52,8 +50,8 @@ config = {
         }
         matchers = new ftf.matchers();
         jasmine.Matchers.prototype.shouldBePresent = matchers.create("ShouldBePresent");
-        
-    },  
+
+    },
     onCleanUp: function(statusCode) {
         if (typeof process.env.__using_grunt === "undefined") {
             try {
@@ -64,11 +62,19 @@ config = {
         }
         console.log("Finished with code:", statusCode);
         console.timeEnd("Tests time");
-    },  
+    },
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 600000
     }
 };
+
+if(_tf_config._system_.seleniumAddress) {
+    config.seleniumAddress = _tf_config._system_.seleniumAddress;
+}
+else {
+    config.chromeOnly = true;
+    config.chromeDriver = '../node_modules/protractor/selenium/chromedriver';
+}
 
 exports.config = config;
