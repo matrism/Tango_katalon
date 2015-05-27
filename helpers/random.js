f36a849e2b3e4a377132c5a4598b04aa81e162d7
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-05-27 18:01:36 -0300

    Memoize non-nullary calls to random.{id,letter} helpers.

--- helpers/random.js
+++ helpers/random.js
@@ -1,12 +1,15 @@
 "use strict";
 var _ = require("lodash");
 var moment = require("moment");
+var memoizeNonNullaryCalls = require('./memoizeNonNullaryCalls');
 exports.id = function() {
 	return Date.now().toString() + Math.floor(Math.random() * 1000);
 };
+exports.id.makeMemoizedGenerator = memoizeNonNullaryCalls.makeFactoryFor(exports.id);
 exports.letter = function() {
     return String.fromCharCode("A".charCodeAt(0) + Math.round(Math.random() * 25));
 };
+exports.letter.makeMemoizedGenerator = memoizeNonNullaryCalls.makeFactoryFor(exports.letter);
 exports.moment = function(a, b) {
 	a = a.valueOf();
 	if(b) {
