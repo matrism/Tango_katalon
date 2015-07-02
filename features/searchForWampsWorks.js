bc8d28aa15498f21ea17a32b692f2e90fefd754f
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-07-02 16:55:57 -0300

    Fix line indentations.

--- features/searchForWampsWorks.js
+++ features/searchForWampsWorks.js
@@ -14,46 +14,46 @@ var beforeFeature = [
             name: 'Search for WAMPS works by full work ID',
             tags: [],
             steps: [
-                [steps.work.selectWorkSearchTagFilter, [0, 'Work ID']],
+                [steps.work.selectWorkSearchFilterTag, [0, 'Work ID']],
 
-				[steps.work.enterWorkSearchTerms, ['WW 008918634 00']],
+                [steps.work.enterWorkSearchTerms, ['WW 008918634 00']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "DON'T UPSET THE RHYTHM"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "DON'T UPSET THE RHYTHM"]],
 
-				[steps.work.enterWorkSearchTerms, ['WW 008887127 00']],
+                [steps.work.enterWorkSearchTerms, ['WW 008887127 00']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "BEYOND THE VEIL"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "BEYOND THE VEIL"]],
 
-				[steps.work.enterWorkSearchTerms, ['WW 008825773 00']],
+                [steps.work.enterWorkSearchTerms, ['WW 008825773 00']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "COLD MOUNTAIN"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "COLD MOUNTAIN"]],
 
-				[steps.work.enterWorkSearchTerms, ['WW 008751880 00']],
+                [steps.work.enterWorkSearchTerms, ['WW 008751880 00']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "BLUE SAILS UPON A SILVER SEA"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "BLUE SAILS UPON A SILVER SEA"]],
             ]
         },
         {
             name: 'Search for WAMPS works by song code',
             tags: [],
             steps: [
-                [steps.work.selectWorkSearchTagFilter, [0, 'Work ID']],
+                [steps.work.selectWorkSearchFilterTag, [0, 'Work ID']],
 
-				[steps.work.enterWorkSearchTerms, ['008918634']],
+                [steps.work.enterWorkSearchTerms, ['008918634']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "DON'T UPSET THE RHYTHM"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "DON'T UPSET THE RHYTHM"]],
 
-				[steps.work.enterWorkSearchTerms, ['008887127']],
+                [steps.work.enterWorkSearchTerms, ['008887127']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "BEYOND THE VEIL"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "BEYOND THE VEIL"]],
 
-				[steps.work.enterWorkSearchTerms, ['008825773']],
+                [steps.work.enterWorkSearchTerms, ['008825773']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "COLD MOUNTAIN"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "COLD MOUNTAIN"]],
 
-				[steps.work.enterWorkSearchTerms, ['008751880']],
+                [steps.work.enterWorkSearchTerms, ['008751880']],
                 [steps.base.waitForAjax],
-				[steps.work.expectWorkSearchMatchTitleToBe, [0, "BLUE SAILS UPON A SILVER SEA"]],
+                [steps.work.expectWorkSearchMatchTitleToBe, [0, "BLUE SAILS UPON A SILVER SEA"]],
             ]
         },
     ];
