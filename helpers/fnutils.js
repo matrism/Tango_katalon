cad8d421a291776c851cb1d52ffeaf6c584cd0d9
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-08-28 13:11:09 -0300

    TAT: Implement fnutils.{bind,using} helpers.

---
+++ helpers/fnutils.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _ = require('lodash');
+
+exports.bind = function(value, fn) {
+    var args = [].slice.call(arguments, 0),
+        bindings = args.slice(0, -1),
+        fn = _.last(args);
+
+    return fn.bind.apply(fn, bindings);
+};
+
+exports.using = function(value, fn) {
+    return exports.bind.apply(null, arguments)();
+};
