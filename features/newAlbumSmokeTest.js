2c056d8a51ab085e423f488fcdc7cbfa95556399
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-08-28 19:33:01 -0300

    TAT-433: Implement work creation.

--- features/newAlbumSmokeTest.js
+++ features/newAlbumSmokeTest.js
@@ -9,6 +9,8 @@ var steps_path = _tf_config._system_.path_to_steps,
 require(steps_path + 'login');
 require(steps_path + 'person');
 require(steps_path + 'newPerson');
+require(steps_path + 'work');
+require(steps_path + 'new_work');
 
 var beforeFeature = [
         [steps.login.itLogin],
@@ -18,6 +20,7 @@ var beforeFeature = [
             name: 'Create person',
             tags: [
                 'new-album-smoke-test-create-person',
+                'new-album-smoke-test-create-work',
             ],
             steps: function() {
                 steps.person.useBlankPersonSlot(0);
@@ -34,6 +37,32 @@ var beforeFeature = [
                 });
 
                 steps.person.findInternalIpiNumber();
+            },
+        },
+        {
+            name: 'Create work',
+            tags: [
+                'new-album-smoke-test-create-work',
+            ],
+            steps: function() {
+                steps.base.useBlankEntityDataSlot('work', 'mainWork');
+
+                using(steps.new_work, function() {
+                    this.goToNewWorkPage();
+
+                    this.enterPrimaryWorkTitle(
+                        'TEST WORK ' + randomId('mainWork')
+                    );
+
+                    this.selectCreatorFromPersonSlot(0, 0);
+                    this.enterCreatorContribution(0, 100);
+
+                    this.optToIncludeWorkOnWebsite(false);
+
+                    this.saveWork();
+                });
+
+                steps.work.findCurrentlyOpenWorkId();
             },
         },
     ];
