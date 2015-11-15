5a2859963ed8389900f7005c333962ac97957c38
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-11-15 06:26:32 -0200

    TAT-480: Add end date and total time informatino to HTML reports.

--- tools/enhanceHtmlReport/index.js
+++ tools/enhanceHtmlReport/index.js
@@ -16,9 +16,15 @@ var fs = require('fs'),
     ));
 
 module.exports = function(path, data) {
-    data.isoStartDate = moment(
-        data.startDate, "YYYY-MM-DD HH-mm-ss"
-    ).toISOString();
+    var startMoment = moment(data.startDate, 'YYYY-MM-DD HH-mm-ss'),
+        endMoment = moment();
+
+    data.isoStartDate = startMoment.toISOString();
+
+    data.endDate = endMoment.format('YYYY-MM-DD HH-mm-ss');
+    data.isoEndDate = endMoment.toISOString();
+
+    data.duration = moment.duration(endMoment.diff(startMoment)).humanize();
 
     fs.appendFileSync(path, template(data));
 };
