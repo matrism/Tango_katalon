9b11a437a8daf3e78e5bd0aba2b976ed52c34280
Eric Colman <eric.colman@wmg.com>
2015-11-16 12:33:49 -0500

    Removed redundant test

--- envs/common/features/works/searchForWampsWorks.js
+++ envs/common/features/works/searchForWampsWorks.js
@@ -8,34 +8,6 @@ exports.commonFeatureTags = ['works', 'smoke', 'searchForWampsWorks'];
 
 exports.feature = [
     {
-        name: 'Search for WAMPS works by full work ID',
-        tags: [],
-        steps: [
-            [steps.base.fail],
-            [steps.work.selectWorkSearchFilterTag, [0, 'Work ID']],
-
-            [steps.work.enterWorkSearchTerms, ['WW 008918634 00']],
-            [steps.base.sleep, [200]],
-            [steps.base.waitForAjax],
-            [steps.work.expectWorkSearchMatchTitleToBe, [0, "DON'T UPSET THE RHYTHM"]],
-
-            [steps.work.enterWorkSearchTerms, ['WW 008887127 00']],
-            [steps.base.sleep, [200]],
-            [steps.base.waitForAjax],
-            [steps.work.expectWorkSearchMatchTitleToBe, [0, "BEYOND THE VEIL"]],
-
-            [steps.work.enterWorkSearchTerms, ['WW 008825773 00']],
-            [steps.base.sleep, [200]],
-            [steps.base.waitForAjax],
-            [steps.work.expectWorkSearchMatchTitleToBe, [0, "COLD MOUNTAIN"]],
-
-            [steps.work.enterWorkSearchTerms, ['WW 008751880 00']],
-            [steps.base.sleep, [200]],
-            [steps.base.waitForAjax],
-            [steps.work.expectWorkSearchMatchTitleToBe, [0, "BLUE SAILS UPON A SILVER SEA"]],
-        ]
-    },
-    {
         name: 'Search for WAMPS works by song code',
         tags: [],
         steps: [
