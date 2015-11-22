5e1e213fe07ce7e63a7bbbe6a2a9b64188f73f92
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-11-22 05:48:48 -0200

    TAT-690: Fix helper loading module (broken file path parsing and globals setting logic).

--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 var path = require('path'),
+    relativePath = path.relative,
+    baseName = path.basename,
     glob = require('glob'),
     recursivelyAccess = require('./recursivelyAccess');
 
 glob.sync(__dirname + '/**/*.js').forEach(function(path) {
-    var pathSlices = path.split('/'),
+    var pathSlices = relativePath(__dirname, path).split('/'),
 
-        helperName = pathSlices.slice(-1)[0],
+        helperName = baseName(pathSlices.slice(-1)[0], '.js'),
 
         helperParent = recursivelyAccess(
             global, pathSlices.slice(0, -1), { create: true }
