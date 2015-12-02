4ddf0894d4a956c4fdb230bed3fbd3422cac5ee4
Guilherme Prá Vieira <n2liquid@users.noreply.github.com>
2015-12-02 18:01:07 -0200

    TAT: Make demo reporter's overlay position fixed.

--- configs/protractor-conf.js
+++ configs/protractor-conf.js
@@ -167,7 +167,7 @@ config = {
                         var $reportWindow = $('body > #TAT_REPORTER');
 
                         if (!$reportWindow.length) {
-                            $reportWindow = $('<div id="TAT_REPORTER" style="pointer-events: none; width: 200px; padding:30px 10px; color:#FFF; background:rgba(0,0,0,0.3); text-align:center; position:absolute; top:0;right:0;"></div>');
+                            $reportWindow = $('<div id="TAT_REPORTER" style="pointer-events: none; width: 200px; padding:30px 10px; color:#FFF; background:rgba(0,0,0,0.3); text-align:center; position:fixed; top:0;right:0;"></div>');
 
                             $('body').append($reportWindow);
                         }
