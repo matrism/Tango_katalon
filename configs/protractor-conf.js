cf05d858c957f8abd6b78cb5cc5dc30a51f47aa0
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-12-02 18:10:12 -0200

    TAT: Make demo reporter overlay show on top of all elements at all times.

--- configs/protractor-conf.js
+++ configs/protractor-conf.js
@@ -167,9 +167,9 @@ config = {
                         var $reportWindow = $('body > #TAT_REPORTER');
 
                         if (!$reportWindow.length) {
-                            $reportWindow = $('<div id="TAT_REPORTER" style="pointer-events: none; width: 200px; padding:30px 10px; color:#FFF; background:rgba(0,0,0,0.3); text-align:center; position:fixed; top:0;right:0;"></div>');
+                            $reportWindow = $('<div id="TAT_REPORTER" style="pointer-events: none; width: 200px; padding:30px 10px; color:#FFF; background:rgba(0,0,0,0.3); text-align:center; position:fixed; top:0;right:0; z-index: 9999;"></div>');
 
-                            $('body').append($reportWindow);
+                            $('body').prepend($reportWindow);
                         }
 
                         $reportWindow.text(result.description);
