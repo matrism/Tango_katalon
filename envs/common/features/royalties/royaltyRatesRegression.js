17ac1b05fc56b1d9635160441f73dffa0a0e7c3f
Guilherme Vieira <guilherme.vieira@wmg.com>
2015-12-18 16:13:53 -0200

    TAT-699: Refactor feature file coding style.

--- envs/common/features/royalties/royaltyRatesRegression.js
+++ envs/common/features/royalties/royaltyRatesRegression.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var using = fnutils.using;
+
+exports.commonFeatureTags = [
+    'royaltyRatesRegression',
+    'royaltyRates',
+    'regression'
+];
+
 exports.beforeFeature = function () {
     steps.login.itLogin();
     steps.createDealGeneral.itFillDealMandatoryFieldsGeneralTab();
@@ -8,199 +16,224 @@ exports.beforeFeature = function () {
     steps.createDealScope.itAddSimpleScope();
 };
 
-exports.commonFeatureTags = [
-    'royaltyRatesRegression',
-    'royaltyRates',
-    'regression'
-];
-
 exports.feature = [
     {
         name: 'Create Royalty Rates Set',
         tags: ['create'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.inspectRateSetForm();
-            steps.royaltyRates.closeRateSetForm();
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.inspectRateSetForm();
+                this.closeRateSetForm();
+            });
         }
     },
     {
         name: 'Create a complex Royalty Rate Set',
         tags: ['create', 'validation'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.clearRoyaltyRateInput();
-            steps.royaltyRates.validateRoyaltyRateInput();
-            steps.royaltyRates.typeIntoRRInput('First Rate Set');
-            steps.royaltyRates.validateRRInputText('First Rate Set');
-            steps.royaltyRates.clearRoyaltyRateInput();
-            steps.royaltyRates.typeIntoRRInput('Really really really really really really really really long rate set name');
-            steps.royaltyRates.validateRRInputText('Really really really really really really really really long rate set name');
-            steps.royaltyRates.validateRRInput();
-            steps.royaltyRates.selectAnIncomeProvider('TEST');
-            steps.royaltyRates.incomeProviderIsPresent('TEST');
-            steps.royaltyRates.incomeDateMethodToggleIsDisplayed();
-            steps.royaltyRates.warnerChappellToggleIsSelected();
-            steps.royaltyRates.selectDealSigningTerritoryToggle();
-            steps.royaltyRates.dealSigningTerritoryIsSelected();
-            steps.royaltyRates.selectWarnerChappellToggle();
-            steps.royaltyRates.warnerChappellToggleIsSelected();
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.clearRoyaltyRateInput();
+                this.validateRoyaltyRateInput();
+                this.typeIntoRRInput('First Rate Set');
+                this.validateRRInputText('First Rate Set');
+                this.clearRoyaltyRateInput();
+                this.typeIntoRRInput('Really really really really really really really really long rate set name');
+                this.validateRRInputText('Really really really really really really really really long rate set name');
+                this.validateRRInput();
+                this.selectAnIncomeProvider('TEST');
+                this.incomeProviderIsPresent('TEST');
+                this.incomeDateMethodToggleIsDisplayed();
+                this.warnerChappellToggleIsSelected();
+                this.selectDealSigningTerritoryToggle();
+                this.dealSigningTerritoryIsSelected();
+                this.selectWarnerChappellToggle();
+                this.warnerChappellToggleIsSelected();
+            });
         }
     },
     {
         name: 'Check Royalty Rate Set Effective Start Date Validation',
         tags: ['validation'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.inspectEffectiveStartDateArea();
-            steps.royaltyRates.checkEffectiveStartDateErrorMessages(
-                [
-                    ['date', 'errorMessage'],
-                    ['12', 'Not a valid date'],
-                    ['00-11-2', 'Invalid Year'],
-                    ['2015-13-2', 'Invalid Month'],
-                    ['2015-11-49', 'Invalid day']
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.inspectEffectiveStartDateArea();
+                this.checkEffectiveStartDateErrorMessages(
+                    [
+                        ['date', 'errorMessage'],
+                        ['12', 'Not a valid date'],
+                        ['00-11-2', 'Invalid Year'],
+                        ['2015-13-2', 'Invalid Month'],
+                        ['2015-11-49', 'Invalid day']
 
-                ],
-                'Check that  %errorMessage% is displayed for date: %date% '
-            );
+                    ],
+                    'Check that  %errorMessage% is displayed for date: %date% '
+                );
+            });
         }
     },
     {
         name: 'Check Royalty Rate Set Rate Applicaiton Method Change',
         tags: ['validation'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.inspectEffectiveStartDateArea();
-            steps.royaltyRates.openEffectiveStartDateCalender();
-            steps.royaltyRates.setEffectiveStartDate('2015-11-11');
-            steps.royaltyRates.addRatePercentageToContractualField('10');
-            steps.royaltyRates.clickOnReceiptApplicationMethod();
-            steps.royaltyRates.confirmChangingRateApplicationMethod();
-            steps.royaltyRates.saveRateSet();
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.inspectEffectiveStartDateArea();
+                this.openEffectiveStartDateCalender();
+                this.setEffectiveStartDate('2015-11-11');
+                this.addRatePercentageToContractualField('10');
+                this.clickOnReceiptApplicationMethod();
+                this.confirmChangingRateApplicationMethod();
+                this.saveRateSet();
+            });
         }
     },
     {
         name: 'Royalty Rate Set save deal',
         tags: ['create', 'edit'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.addRatePercentageToContractualField('10');
-            steps.royaltyRates.addIncomeProviderByPartialMatch('HFA');
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.addRatePercentageToContractualField('10');
+                this.addIncomeProviderByPartialMatch('HFA');
 
-            steps.royaltyRates.clickOnReceiptApplicationMethod();
-            steps.royaltyRates.confirmChangingRateApplicationMethod();
-            steps.royaltyRates.storeRRData();
+                this.clickOnReceiptApplicationMethod();
+                this.confirmChangingRateApplicationMethod();
+                this.storeRRData();
 
-            //steps.royaltyRates.saveRateSet();
-            //
-            steps.deal.itContinueToNextPage();
-            steps.deal.saveDeal();
-            steps.deal.clickFirstScopeHeader();
-            steps.royaltyRates.verifyRateSetSavedData();
+                using(steps.deal, function () {
+                    this.itContinueToNextPage();
+                    this.saveDeal();
+                    this.clickFirstScopeHeader();
+                });
+
+                this.verifyRateSetSavedData();
+            });
         }
     },
     {
         name: 'Royalty Rate Set can be saved with errors',
         tags: ['create', 'validation'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.addRatePercentageToContractualField('10');
-            steps.royaltyRates.addIncomeProviderByPartialMatch('HFA');
-            steps.royaltyRates.addEffectiveStartDate('2-2-2');
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.addRatePercentageToContractualField('10');
+                this.addIncomeProviderByPartialMatch('HFA');
+                this.addEffectiveStartDate('2-2-2');
 
-            steps.royaltyRates.storeRRData();
+                this.storeRRData();
 
-            steps.royaltyRates.saveRateSet();
+                this.saveRateSet();
 
-            steps.base.waitForModal();
-            steps.base.validateModalMessageBody('Add an Effective Date to ensure activation of rates');
-            steps.base.closeModal();
+                using(steps.base, function () {
+                    this.waitForModal();
 
-            steps.deal.clickFirstScopeHeader();
-            steps.royaltyRates.verifyRateSetSavedData();
+                    this.validateModalMessageBody(
+                        'Add an Effective Date to ensure activation of rates'
+                    );
 
-            steps.deal.verifyErrorMessages();
+                    this.closeModal();
+                });
+
+                steps.deal.clickFirstScopeHeader();
+
+                this.verifyRateSetSavedData();
+
+                steps.deal.verifyErrorMessages();
+            });
         }
     },
     {
         name: 'Royalty Rate Set can be saved from other deal modules',
         tags: ['create', 'edit', 'validation'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.addRatePercentageToContractualField('10');
-            steps.royaltyRates.addIncomeProviderByPartialMatch('HFA');
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.addRatePercentageToContractualField('10');
+                this.addIncomeProviderByPartialMatch('HFA');
 
-            steps.royaltyRates.clickOnReceiptApplicationMethod();
-            steps.royaltyRates.confirmChangingRateApplicationMethod();
+                this.clickOnReceiptApplicationMethod();
+                this.confirmChangingRateApplicationMethod();
 
-            steps.royaltyRates.saveRateSet();
+                this.saveRateSet();
 
-            steps.deal.itContinueToNextPage();
-            steps.deal.saveDeal();
-            steps.deal.clickFirstScopeHeader();
+                using(steps.deal, function () {
+                    this.itContinueToNextPage();
+                    this.saveDeal();
+                    this.clickFirstScopeHeader();
+                });
 
-            steps.royaltyRates.editSingleRoyaltySet();
-            steps.editRoyaltyRates.openRateSetPanel();
+                this.editSingleRoyaltySet();
 
-            steps.royaltyRates.clearRoyaltyRateInput();
-            steps.royaltyRates.typeIntoRRInput('Edited RR Set');
-            steps.royaltyRates.editIncomeProviderByPartialMatch('ASCAP');
-            steps.royaltyRates.addEffectiveStartDate('2019-05-26');
+                steps.editRoyaltyRates.openRateSetPanel();
 
-            steps.royaltyRates.saveRRData();
+                this.clearRoyaltyRateInput();
+                this.typeIntoRRInput('Edited RR Set');
+                this.editIncomeProviderByPartialMatch('ASCAP');
+                this.addEffectiveStartDate('2019-05-26');
 
-            steps.royaltyRates.addNewPublisherShares();
-            steps.royaltyRates.addOriginalPublisherToPublisherShares('ASCAP');
+                this.saveRRData();
 
-            steps.royaltyRates.addAdministratorToPublisherShares('ASCAP');
-            steps.royaltyRates.savePublisherShares();
+                this.addNewPublisherShares();
+                this.addOriginalPublisherToPublisherShares('ASCAP');
 
-            steps.deal.clickFirstScopeHeader();
-            steps.royaltyRates.verifyRateSetSavedData();
+                this.addAdministratorToPublisherShares('ASCAP');
+                this.savePublisherShares();
+
+                steps.deal.clickFirstScopeHeader();
+
+                this.verifyRateSetSavedData();
+            });
         }
     },
     {
         name: 'Saving RR Set Saves whole Deal',
         tags: ['create', 'edit', 'validation'],
         steps: function () {
-            steps.royaltyRates.addNewRoyaltySet();
-            steps.royaltyRates.addRatePercentageToContractualField('10');
-            steps.royaltyRates.addIncomeProviderByPartialMatch('HFA');
+            using(steps.royaltyRates, function () {
+                this.addNewRoyaltySet();
+                this.addRatePercentageToContractualField('10');
+                this.addIncomeProviderByPartialMatch('HFA');
 
-            steps.royaltyRates.clickOnReceiptApplicationMethod();
-            steps.royaltyRates.confirmChangingRateApplicationMethod();
+                this.clickOnReceiptApplicationMethod();
+                this.confirmChangingRateApplicationMethod();
 
-            steps.royaltyRates.saveRateSet();
+                this.saveRateSet();
 
-            steps.deal.itContinueToNextPage();
-            steps.deal.saveDeal();
-            steps.deal.clickFirstScopeHeader();
+                using(steps.deal, function () {
+                    this.itContinueToNextPage();
+                    this.saveDeal();
+                    this.clickFirstScopeHeader();
+                });
 
-            steps.royaltyRates.editSingleRoyaltySet();
-            steps.editRoyaltyRates.openRateSetPanel();
+                this.editSingleRoyaltySet();
 
-            steps.royaltyRates.clearRoyaltyRateInput();
-            steps.royaltyRates.typeIntoRRInput('Edited RR Set');
-            steps.royaltyRates.editIncomeProviderByPartialMatch('ASCAP');
-            steps.royaltyRates.addEffectiveStartDate('2019-05-26');
+                steps.editRoyaltyRates.openRateSetPanel();
 
-            steps.royaltyRates.saveRRData();
+                this.clearRoyaltyRateInput();
+                this.typeIntoRRInput('Edited RR Set');
+                this.editIncomeProviderByPartialMatch('ASCAP');
+                this.addEffectiveStartDate('2019-05-26');
 
-            steps.royaltyRates.addNewPublisherShares();
-            steps.royaltyRates.addOriginalPublisherToPublisherShares('ASCAP');
+                this.saveRRData();
 
-            steps.royaltyRates.addAdministratorToPublisherShares('ASCAP');
+                this.addNewPublisherShares();
+                this.addOriginalPublisherToPublisherShares('ASCAP');
 
-            steps.royaltyRates.saveRateSet();
+                this.addAdministratorToPublisherShares('ASCAP');
 
-            steps.royaltyRates.waitForAjaxCall();
-            steps.royaltyRates.refreshPage();
+                this.saveRateSet();
 
-            steps.royaltyRates.openSavedScope();
+                this.waitForAjaxCall();
+                this.refreshPage();
 
-            steps.royaltyRates.verifyRateSetSavedData();
-            steps.royaltyRates.verifyPublisherShare();
+                this.openSavedScope();
+
+                this.verifyRateSetSavedData();
+                this.verifyPublisherShare();
+            });
         }
     }
 ];
