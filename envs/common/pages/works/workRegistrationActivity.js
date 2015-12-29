c5c526a59bd7ec7ff219245b34dc47b7329e2a47
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2015-12-29 18:10:24 -0200

    TAT-386: fixing work registration activity

--- envs/common/pages/works/workRegistrationActivity.js
+++ envs/common/pages/works/workRegistrationActivity.js
@@ -88,6 +88,7 @@ exports.activityGroup = (function() {
         var target = activityGroup.targets[targetGroupName || 'latest'];
 
         pages.base.scrollIntoView(target.container);
+        pages.base.waitForAjax();
 
         browser.wait(ExpectedConditions.elementToBeClickable(
             activityGroup.container.all().last()
