d80fb74222c025cd45eb18e831eea108723e17c5
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2015-12-29 11:26:42 -0200

    TAT-386: fixing work activity issue

--- envs/common/features/orgs/CrRegistration.js
+++ envs/common/features/orgs/CrRegistration.js
@@ -55,6 +55,7 @@ exports.feature = [
 
             using(steps.workRegistrationActivity.activityGroup, function() {
                 this.find({ firstWithRecipientName: 'BMI' });
+                steps.base.sleep(5000);
                 this.toggleBlind();
                 using(this.events, function() {
                     this.find({ firstWithFileName: fromTestVariable('last event file name') });
