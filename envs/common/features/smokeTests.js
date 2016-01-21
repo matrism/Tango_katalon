6ebeaef31eb5cfbedb66d2279728e704ba585228
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-01-21 17:11:38 -0200

    Fix crFileDownloadsSmoke test tags.

--- envs/common/features/smokeTests.js
+++ envs/common/features/smokeTests.js
@@ -158,7 +158,14 @@ exports.feature = [
     ,
     {
         name: 'CR file downloads',
-        tags: ['orgs', 'crFileDownloads'],
+
+        tags: [
+            'crFileDownloadsSmoke',
+            'crFileDownloads',
+            'cr',
+            'orgs'
+        ],
+
         steps: function () {
             steps.base.clearDownloadsDirectory();
             steps.searchSection.accessSavedOrganisationByName('BMI');
