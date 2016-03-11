28b04a734c99fb77f7891f683e2c5f90369895d0
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2016-03-11 18:00:16 -0300

    TAT-754: LAD script - add RTP sets

---
+++ envs/common/features/deals/rtpLad.js
@@ -0,0 +1,48 @@
+'use strict';
+
+exports.beforeFeature = function () {
+    steps.login.itLogin();
+},
+
+exports.commonFeatureTags = ['deals', 'dealLad', 'regression'];
+
+exports.feature = [
+    {
+        name: 'Create a basic deal for LAD with RTP',
+        tags: ['rtpLad'],
+        steps: function () {
+
+            var timeout = 100000,
+                countPtc = 0;
+            steps.searchSection.accessSavedDealByNumber('267945');
+
+            steps.deal.goToTermsDealTabDetails();
+            steps.deal.goToRightsTermPeriodsTermsTabDetails();
+
+            for (var acq = 0; acq <= 2; acq++) {
+                countPtc = 0;
+                //if (acq != 0) {
+                steps.editDealRtp.editClickOnAddAnotherAcquisitionPeriodLink();
+                steps.editDealRtp.selectScopeNumberIFromInput(0, 0, 'acq');
+                //}
+
+                for (var ret = 0; ret <= 1; ret++) {
+                    steps.editDealRtp.clickOnAddRetentionFromAcquisitionLink();
+                    steps.editDealRtp.selectScopeNumberIFromInput(0, ret, 'ret');
+                    steps.editDealRtp.selectScopeNumberIFromInput(1, ret, 'ret');
+                    steps.editDealRtp.editSelectSpecificDurationTypeRetentionFromAcquisitionNumberI(ret + 1, 'Life of Copyright');
+                    for (var ptc = 0; ptc <= 1; ptc++) {
+                        steps.editDealRtp.clickOnAddPostTermCollectionFromRetention(ret);
+                        steps.editDealRtp.selectScopeNumberIFromInput(0, countPtc, 'ptc');
+                        steps.editDealRtp.selectScopeNumberIFromInput(1, countPtc, 'ptc');
+                        steps.editDealRtp.editFillIntoDurationFieldPostTermCollectionFromRetention(ret, ptc);
+                        countPtc++;
+                    }
+                }
+
+                steps.editDealRtp.editSaveAnotherAcquisitionForm();
+                //steps.editDealRtp.saveRetentionFromAcquisition();
+            }
+        }
+    }
+]
