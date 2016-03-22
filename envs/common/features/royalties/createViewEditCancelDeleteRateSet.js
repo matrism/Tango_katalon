77dd062ac7fe640892307072856af7b820e217a4
denisafericean <denisafericean>
2016-03-22 16:51:02 +0200

    TAT-160

--- envs/common/features/royalties/createViewEditCancelDeleteRateSet.js
+++ envs/common/features/royalties/createViewEditCancelDeleteRateSet.js
@@ -4,7 +4,7 @@ exports.beforeFeature = function () {
     steps.login.itLogin();
 };
 
-exports.commonFeatureTags = ['royaltyRates', 'rr', 'regression'];
+exports.commonFeatureTags = ['royaltyRates', 'rateSet', 'regression'];
 
 exports.feature = [
     {
