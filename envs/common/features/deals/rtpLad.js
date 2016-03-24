d4c63716ff54c650e66fa3f68ef680f771d36620
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2016-03-24 17:47:05 -0300

    Revert "TAT-754: remove lad from nightly run"
    This reverts commit 477b7756a08734b9c4f5e48bd60bae56bb8ceb11.

--- envs/common/features/deals/rtpLad.js
+++ envs/common/features/deals/rtpLad.js
@@ -4,7 +4,7 @@ exports.beforeFeature = function () {
     steps.login.itLogin();
 },
 
-exports.commonFeatureTags = ['dealLad'];
+exports.commonFeatureTags = ['deals', 'dealLad', 'regression'];
 
 exports.feature = [
     {
