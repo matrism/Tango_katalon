75ed1839ff3fa1e46188a07e34139b4709d24cd7
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2016-03-30 21:28:01 -0300

    TAT-814: LAD - testing with less repetitions

--- envs/common/features/deals/lad.js
+++ envs/common/features/deals/lad.js
@@ -288,8 +288,8 @@ exports.feature = [
             edp.editFillIntoPayeeDistributionInputField();
             edp.editSavePayeeToPayeeForm();
 
-            for (var j = 2; j <= 200; j++) {
-            //for (var j = 2; j <= 10; j++) {
+            //for (var j = 2; j <= 200; j++) {
+            for (var j = 2; j <= 10; j++) {
                 edp.editSelectSpecificNewPayeePersonFromDropDown('person ' + j + ', TAT payee');
                 for (var i = 3 * j; i >= 3 * j - 2; i--) {
                     edp.editAssociateSpecificScopeNumberIToNewPayee(i);
@@ -315,8 +315,8 @@ exports.feature = [
             steps.deal.goToTermsDealTabDetails();
             steps.deal.goToRightsTermPeriodsTermsTabDetails();
 
-            for (var acq = 0; acq <= 200; acq++) {
-            //for (var acq = 0; acq <= 10; acq++) {
+            //for (var acq = 0; acq <= 200; acq++) {
+            for (var acq = 0; acq <= 10; acq++) {
                 countPtc = 0;
                 steps.editDealRtp.editClickOnAddAnotherAcquisitionPeriodLink();
                 steps.editDealRtp.selectScopeNumberIFromInput(0, 0, 'acq');
