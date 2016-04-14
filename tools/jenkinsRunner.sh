9d24664b2c711fef01538843b40bcc612643e12f
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-04-14 15:18:30 -0300

    TAT-834: Update jenkinsRunner to use jobRunner.

--- tools/jenkinsRunner.sh
+++ tools/jenkinsRunner.sh
@@ -5,8 +5,8 @@ echo "Using node `node -v`"
 echo "Using `google-chrome --version`"
 
 mkdir -p reports/html
-chmod 755 start.sh
 
+# Dependency setup.
 npm install
 
 # Newer versions of Protractor require versions of chromedriver (and therefore Chrome)
@@ -18,9 +18,8 @@ echo '{"webdriverVersions": {"selenium": "2.47.1", "chromedriver": "2.14", "iedr
 ./node_modules/protractor/bin/webdriver-manager update \
     --out_dir ./node_modules/protractor/selenium --standalone false
 
-cmd_line="xvfb-run -a -s \"-screen 0 1920x1080x24\""
-
-cmd_line+=" ./node_modules/.bin/protractor configs/protractor-conf.js"
+# Job runner.
+cmd_line="node ./tools/jobRunner.js $job_runner_options"
 
 if [ -n "$app_url" ]; then
     cmd_line+=" --app-url $(printf '%q' "$app_url")"
@@ -38,14 +37,6 @@ if [ -n "$negated_test_tags" ]; then
     cmd_line+=" --@tags $(printf '%q' "$negated_test_tags")"
 fi
 
-if [ -n "$wait_timeout" ]; then
-    cmd_line+=" --wait_timeout $(printf '%q' "$wait_timeout")"
-fi
-
-if [ -n "$test_cycle_name" ]; then
-    cmd_line+=" --tcn $(printf '%q' "$test_cycle_name") "
-fi
-
 if [ -n "$branch" ]; then
     cmd_line+=" --branch $(printf '%q' "$branch")"
 fi
@@ -54,8 +45,29 @@ cmd_line+=" --commit $(git rev-parse HEAD)"
 
 cmd_line+=" --build $(printf '%q' "$BUILD_NUMBER")"
 
+if [ -n "$job_silence_timeout" ]; then
+    cmd_line+=" --jobSilenceTimeout $(printf '%q' "$job_silence_timeout")"
+fi
+
+# Xvfb.
+cmd_line+=" xvfb-run -a -s \"-screen 0 1920x1080x24\""
+
+# Protractor.
+cmd_line+=" ./node_modules/.bin/protractor configs/protractor-conf.js"
+
+if [ -n "$wait_timeout" ]; then
+    cmd_line+=" --wait_timeout $(printf '%q' "$wait_timeout")"
+fi
+
+if [ -n "$test_cycle_name" ]; then
+    cmd_line+=" --tcn $(printf '%q' "$test_cycle_name") "
+fi
+
 cmd_line+=" $additional_protractor_options"
 
-echo "> $cmd_line"
+# Report logging.
+cmd_line+=" | tee reports/html/testing/report.txt"
 
+# Invocation
+echo "> $cmd_line"
 eval $cmd_line
