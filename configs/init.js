249ec072c7af62b426f1b00809b5b1ce7102955b
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-04-20 17:28:45 -0300

    TAT-834: Fix feature enumeration code.
    Overrides weren't being taken into account and tag matching was broken.

--- configs/init.js
+++ configs/init.js
@@ -1,73 +1,29 @@
 'use strict';
 
-require('array.prototype.includes');
+let enumFeatures = require('../tools/enumFeatures'),
 
-let path = require('path'),
-    glob = require('glob'),
-
-    rootDir = path.normalize(__dirname + '/..'),
-
-    fMods = (() => {
-        if(systemConfig.feat) {
-            return [rootDir + '/' + systemConfig.feat];
-        }
-        else {
-            return glob.sync(rootDir + '/envs/**/features/**/*.js');
-        }
-    })();
+    features = enumFeatures(
+        systemConfig.env.name, systemConfig.tags, systemConfig.tags.negated
+    );
 
 global.featureList = [];
 global.scenarioList = [];
 
-function tagMatches(a, b) {
-    return a.some((a) => {
-        return b.includes(a);
-    });
-}
-
-fMods.forEach((fPath) => {
-    let fMod = require(fPath);
+features.forEach((feature) => {
+    let fMod = feature.module;
 
     // Not a feature module? Skip.
     if(!fMod.feature) {
         return;
     }
 
-    let shortPath = fPath.slice(rootDir.length + 1),
-
-        fTags = fMod.commonFeatureTags || [];
-
-    if(tagMatches(fTags, systemConfig.tags.negated)) {
-        return;
-    }
-
-    if(systemConfig.tags.length > 0) {
-        let fAllTags = fTags.slice(0);
-
-        fMod.feature.forEach((scenario) => {
-            fAllTags.push(...scenario.tags);
-        });
-
-        if(!tagMatches(fAllTags, systemConfig.tags)) {
-            return;
-        }
-    }
-
     featureList.push(fMod);
 
-    describe(shortPath, () => {
+    describe(feature.relPath, () => {
         fMod.feature.forEach((scenario) => {
+            scenarioList.push(scenario);
+
             describe(scenario.name, () => {
-                scenarioList.push(scenario);
-
-                if(systemConfig.tags.length > 0) {
-                    let tags = fMod.commonFeatureTags.concat(scenario.tags);
-
-                    if(!tagMatches(tags, systemConfig.tags)) {
-                        return;
-                    }
-                }
-
                 if(fMod.beforeFeature) {
                     describe('Before feature', () => {
                         fMod.beforeFeature();
---
+++ tools/enumFeatures.js
@@ -0,0 +1,89 @@
+'use strict';
+
+require('array.prototype.includes');
+
+let path = require('path'),
+    glob = require('glob'),
+
+    _ = require('lodash'),
+
+    rootDir = path.normalize(__dirname + '/..');
+
+function tagMatches(a, b) {
+    return a.some((a) => {
+        return b.includes(a);
+    });
+}
+
+module.exports = (envType, tags, negatedTags) => {
+    let ret = [],
+
+        overrideCandidates = glob.sync(
+            rootDir + '/envs/' + envType + '/features/**/*.js'
+        ),
+
+        candidates = glob.sync(rootDir + '/envs/common/features/**/*.js').filter((path) => {
+            let tail = path.slice((rootDir + '/envs/common/features/').length),
+
+                hasNoOverride = (overrideCandidates.indexOf(
+                    rootDir + '/envs/' + envType + '/features/' + tail
+                ) === -1);
+
+            return hasNoOverride;
+        }),
+
+        allCandidates = overrideCandidates.concat(candidates);
+
+    allCandidates.forEach((fPath) => {
+        let fMod = _.cloneDeep(require(fPath));
+
+        // Skip if not a feature module.
+        if(!fMod.feature) {
+            return;
+        }
+
+        let fTags = fMod.commonFeatureTags || [];
+
+        // Skip if any of the feature tags are being negated.
+        if(tagMatches(fTags, negatedTags)) {
+            return;
+        }
+
+        // Feature file is explicitly matched if there are no requested tags
+        // or one or more of the requested tags match a feature tag.
+        let fExplicitlyMatched = tags.length === 0 || tagMatches(fTags, tags);
+
+        // For each feature scenario...
+        fMod.feature = fMod.feature.filter((scenario) => {
+            let sTags = scenario.tags || [];
+
+            // Skip scenario if any of its tags are being negated,
+            // even if the feature file was explicitly matched.
+            if(tagMatches(sTags, negatedTags)) {
+                return false;
+            }
+
+            // Skip scenario if feature file wasn't explicitly matched, and
+            // there are requested tags, and none of them matches the
+            // scenario tags.
+            if(!fExplicitlyMatched && tags.length > 0 && !tagMatches(sTags, tags)) {
+                return false;
+            }
+
+            return true;
+        });
+
+        // Skip feature file if all scenarios have been filtered out.
+        if(fMod.feature.length === 0) {
+            return;
+        }
+
+        ret.push({
+            path: fPath,
+            relPath: fPath.slice(rootDir.length + 1),
+            module: fMod
+        });
+    });
+
+    return ret;
+};
