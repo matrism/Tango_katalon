8ba91177268ffd8eb39ec12afe0313fdf79a63fd
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-04-20 19:45:01 -0300

    TAT-834: Implement broken tag handling.

--- configs/init.js
+++ configs/init.js
@@ -19,11 +19,39 @@ features.forEach((feature) => {
 
     featureList.push(fMod);
 
+    let skipBroken = !systemConfig.dontSkipBroken,
+
+        noBreakageDescriptionMsg = 'No breakage description provided';
+
     describe(feature.relPath, () => {
+        let fTags = fMod.commonFeatureTags || [];
+
+        if(skipBroken && fTags.includes('broken')) {
+            it('Broken feature', () => {
+                throw new Error(
+                    fMod.breakageDescription || noBreakageDescriptionMsg
+                );
+            });
+
+            return;
+        }
+
         fMod.feature.forEach((scenario) => {
             scenarioList.push(scenario);
 
             describe(scenario.name, () => {
+                let sTags = scenario.tags || [];
+
+                if(skipBroken && sTags.includes('broken')) {
+                    it('Broken scenario', () => {
+                        throw new Error(
+                            scenario.breakageDescription || noBreakageDescriptionMsg
+                        );
+                    });
+
+                    return;
+                }
+
                 if(fMod.beforeFeature) {
                     describe('Before feature', () => {
                         fMod.beforeFeature();
