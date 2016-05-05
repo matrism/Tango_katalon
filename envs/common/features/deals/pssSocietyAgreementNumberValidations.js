5a5bb7a574c5f7764e77a762520d474e29a21387
denisafericean <denisafericean>
2016-05-05 17:14:23 +0300

    TAT-829

--- envs/common/features/deals/pssSocietyAgreementNumberValidations.js
+++ envs/common/features/deals/pssSocietyAgreementNumberValidations.js
@@ -39,9 +39,9 @@ exports.feature = [
             steps.editDealScope.checkCreatorToPublisherChainSocietyAgreementNumberTextTooltip();
             steps.editDealScope.checkPublisherChainAgreementNumbersSocietyAgreementNumberTextTooltip();
             //validate tooltips delete icon
-            steps.editDealScope.checkDeleteCreatorSetSocietyAgreementNumberTextTooltip();
-            steps.editDealScope.checkDeleteAgreementNumberSocietyAgreementNumberTextTooltip();
-            steps.editDealScope.checkDeletePublisherChainAgreementNumberSocietyAgreementNumberTextTooltip();
+            //steps.editDealScope.checkDeleteCreatorSetSocietyAgreementNumberTextTooltip();
+            //steps.editDealScope.checkDeleteAgreementNumberSocietyAgreementNumberTextTooltip();
+            //steps.editDealScope.checkDeletePublisherChainAgreementNumberSocietyAgreementNumberTextTooltip();
 
             //save the society agreement number
             steps.editDealScope.saveChangesSocietyAgreementNumberForm();
