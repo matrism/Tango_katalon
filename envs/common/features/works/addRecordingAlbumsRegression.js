0d6ae1961911446c226b60241da6433a9129017c
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-05-13 03:12:07 -0300

    TAT-208: Implement test flows.

---
+++ envs/common/features/works/addRecordingAlbumsRegression.js
@@ -0,0 +1,369 @@
+'use strict';
+
+let leftPad = require('left-pad');
+
+exports.commonFeatureTags = [
+    'addWorkRecordingAlbumsRegression',
+    'addWorkRecordingAlbums',
+    'workRecordingAlbumsRegression',
+    'workRecordingAlbums',
+    'workRecordingsRegression',
+    'workRecordings',
+    'works',
+    'albums',
+    'regresion'
+];
+
+let test = {};
+
+test.album = {};
+test.person = {};
+test.work = {};
+
+exports.beforeFeature = () => {
+    let login = steps.login,
+        base = steps.base;
+
+    // ---
+
+    login.itLogin();
+
+    // ---
+
+    test.id = random.id();
+
+    test.album.title = `TEST ALBUM ${test.id}`;
+
+    test.album.releaseDetails = _.times(4, i => {
+        let ret = {};
+
+        ret.id = random.id();
+
+        ret.territories = [[
+            'United States', 'Brazil', 'India', 'Poland'
+        ][i]];
+
+        {
+            let iPadded = leftPad(i, 2, 0);
+
+            ret.releaseDate = [
+                2000 + i, iPadded, iPadded
+            ].join('-');
+        }
+
+        ret.configuration = [
+            'CD', 'LP', 'Digital', 'Hard Drive'
+        ][i];
+
+        ret.labelName = `TEST LABEL ${ret.id}`;
+
+        ret.catalogueNumber = ret.id.slice(0, 15);
+
+        ret.licenseCode = `LICENSE${ret.id}`;
+
+        return ret;
+    });
+
+    test.person.name = `TEST PERSON ${test.id}`;
+
+    test.work.title = `TEST WORK ${test.id}`;
+
+    // ---
+
+    describe('Create albums', () => {
+        let na = steps.newAlbum,
+            nard = na.releaseDetails;
+
+        // ---
+
+        base.useBlankEntityDataSlot('album', 'main');
+
+        na.goToNewAlbumPage();
+
+        na.enterTitle(test.album.title);
+
+        na.selectReleaseType('Commercial');
+
+        na.enterArtistSearchTerms(`TEST ARTIST ${test.id}`);
+        na.createEnteredArtist();
+
+        na.enterAlbumCode(`TESTALBUMCODE${test.id}`);
+
+        // ---
+
+        na.goToTab('Release Details');
+
+        test.album.releaseDetails.forEach((row, i) => {
+            nard.waitForTerritoriesSelectorToBeReady(i);
+            nard.editTerritories(i);
+
+            row.territories.forEach((val, j) => {
+                describe(`Add territory ${j}`, () => {
+                    nard.enterTerritorySearchTerms(i, val);
+                    nard.selectTerritorySearchResultByIndex(0, val);
+                });
+            });
+
+            nard.enterReleaseDate(i, row.releaseDate);
+
+            nard.selectConfiguration(i, row.configuration);
+
+            nard.enterLabelSearchTerms(i, row.labelName);
+            nard.createEnteredLabel();
+
+            nard.enterCatalogueNumber(i, row.catalogueNumber);
+
+            nard.enterLicenseCode(i, row.licenseCode);
+        });
+
+        // ---
+
+        na.save();
+    });
+
+    // ---
+
+    describe('Create person', () => {
+        let p = steps.person,
+            np = steps.newPerson;
+
+        // ---
+
+        p.useBlankPersonSlot(0);
+
+        np.goToNewPersonPage();
+
+        np.enterLastName(test.person.name);
+
+        np.enterAffiliatedSocietySearchTerms('BMI');
+        np.selectAffiliatedSocietySearchResultByIndex(0, 'BMI');
+
+        np.save();
+    });
+
+    describe('Fill required work creation fields', () => {
+        let nw = steps.newWork;
+
+        // ---
+
+        base.useBlankEntityDataSlot('work', 'main');
+
+        nw.goToNewWorkPage();
+
+        nw.enterPrimaryWorkTitle(test.work.title);
+
+        nw.enterCreatorSearchTerms(0, test.id);
+        nw.selectCreatorSearchResultByIndex(0, test.person.name);
+
+        nw.enterCreatorContribution(0, 100);
+
+        nw.optToIncludeWorkOnWebsite(false);
+    });
+};
+
+let addRecordings = () => {
+    let wr = steps.workRecordings,
+        alb = wr.albums;
+
+    describe('Add recordings', () => {
+        test.recRows = _.times(4, i => {
+            describe(`Add recording ${i}`, () => {
+                let recRow = {};
+
+                recRow.id = random.id();
+
+                recRow.title = `TEST RECORDING ${recRow.id}`;
+
+                recRow.artist = {};
+
+                recRow.artist.id = random.id();
+                recRow.artist.name = `TEST ARTIST ${recRow.artist.id}`;
+
+                // ---
+
+                wr.enterTitle(i, recRow.title);
+                wr.validateEnteredTitle(i, recRow.title);
+
+                wr.enterArtistSearchTerms(i, recRow.artist.name);
+                wr.createEnteredArtist();
+                wr.validateEnteredArtistSearchTerms(i, recRow.artist.name);
+
+                wr.validateRemovability(i, true);
+
+                // ---
+
+                alb.toggleExpand(i);
+
+                alb.enterSearchTerms(i, 0, test.id);
+                alb.selectSearchResultByIndex(0, test.album.title);
+                alb.validateSelectedAlbumTitle(i, 0, test.album.title);
+
+                wr.validateRemovability(i, false);
+
+                wr.validateRemoveButtonTooltip(
+                    i, 'Cannot Delete a Recording while associated to an Album'
+                );
+
+                alb.enterTrackNumber(i, 0, i + 1);
+                alb.validateEnteredTrackNumber(i, 0, i + 1);
+
+                return recRow;
+            });
+        });
+    });
+};
+
+let validateRecordingIrremovabilities = () => {
+    let wr = steps.workRecordings;
+
+    describe('Validate recording irremovabilities', () => {
+        test.recRows.forEach((row, i) => {
+            wr.validateRemoveButtonTooltip(
+                i, 'Cannot Delete a Recording while associated to an Album'
+            );
+
+            wr.validateRemovability(i, false);
+        });
+    });
+};
+
+let validateRecordingAlbumReleaseDetails = () => {
+    let wr = steps.workRecordings,
+        alb = wr.albums,
+        rd = alb.releaseDetails;
+
+    describe('Validate recording album release details', () => {
+        test.recRows.forEach((row, i) => {
+            describe(`Recording ${i}`, () => {
+                alb.toggleExpand(i);
+                rd.toggleExpand(0);
+
+                // ---
+
+                test.album.releaseDetails.forEach((row, i) => {
+                    describe(`Release details row ${i}`, () => {
+                        rd.validateTerritories(i, row.territories);
+                        rd.validateReleaseDate(i, row.releaseDate);
+                        rd.validateConfiguration(i, row.configuration);
+                        rd.validateLabelName(i, row.labelName);
+                        rd.validateCatalogueNumber(i, row.catalogueNumber);
+                        rd.validateLicenseCode(i, row.licenseCode);
+                    });
+                });
+            });
+        });
+    });
+};
+
+let openAlbum = () => {
+    let mhs = steps.mainHeader.search;
+
+    // ---
+
+    mhs.selectEntityType('Albums');
+
+    mhs.enterTerms(test.id);
+    mhs.selectResultByIndex(0, test.album.title);
+};
+
+let validateAlbumRecordings = () => {
+    let alb = steps.album,
+        ah = alb.header,
+        ar = alb.recordings;
+
+    // ---
+
+    ah.validateTrackCount(test.recRows.length);
+
+    describe('Validate recordings', () => {
+        test.recRows.forEach((recRow, i) => {
+            describe(`Recording ${i}`, () => {
+                ar.validateTrackNumber(i, i + 1);
+                ar.validateTitle(i, recRow.title);
+                ar.validateArtistName(i, recRow.artist.name);
+            });
+        });
+    });
+};
+
+exports.feature = [
+    {
+        name: 'Add recordings and associate to album (create mode)',
+
+        tags: [],
+
+        steps: () => {
+            let nw = steps.newWork,
+                w = steps.work;
+
+            // ---
+
+            nw.continueToNextTab();
+
+            addRecordings();
+
+            nw.save();
+
+            // ---
+
+            w.goToRecordingsTab();
+
+            validateRecordingIrremovabilities();
+
+            validateRecordingAlbumReleaseDetails();
+
+            // ---
+
+            openAlbum();
+
+            validateAlbumRecordings();
+        }
+    },
+
+    {
+        name: 'Add recordings and associate to album (edit mode)',
+
+        tags: [],
+
+        steps: () => {
+            let base = steps.base,
+                nw = steps.newWork,
+                w = steps.work,
+                wr = steps.workRecordings;
+
+            // ---
+
+            nw.save();
+
+            // ---
+
+            w.goToRecordingsTab();
+
+            wr.addRecordings();
+
+            addRecordings();
+
+            wr.save();
+
+            // ---
+
+            validateRecordingIrremovabilities();
+
+            validateRecordingAlbumReleaseDetails();
+
+            // ---
+
+            base.refreshPage();
+
+            validateRecordingIrremovabilities();
+
+            validateRecordingAlbumReleaseDetails();
+
+            // ---
+
+            openAlbum();
+
+            validateAlbumRecordings();
+        }
+    }
+];
---
+++ envs/common/features/works/removeRecordings.js
@@ -0,0 +1,384 @@
+'use strict';
+
+let leftPad = require('left-pad');
+
+let randomId = random.id.makeMemoizedGenerator();
+
+exports.commonFeatureTags = [
+    'removeWorkRecordingsRegression',
+    'removeWorkRecordings',
+    'workRecordingsRegression',
+    'workRecordings',
+    'works',
+    'regression'
+];
+
+let test = {};
+
+test.scenario = {};
+test.person = {};
+test.work = {};
+
+test.scenario.i = 0;
+
+exports.beforeFeature = () => {
+    steps.login.itLogin();
+
+    ++test.scenario.i;
+
+    test.scenario.id = randomId(test.scenario.i);
+
+    test.person.name = `TEST PERSON ${test.scenario.id}`;
+    test.work.title = `TEST WORK ${test.scenario.id}`;
+
+    describe('Create person', () => {
+        let p = steps.person;
+
+        p.useBlankPersonSlot(0);
+
+        // ---
+
+        let np = steps.newPerson;
+
+        np.goToNewPersonPage();
+
+        np.enterLastName(test.person.name);
+
+        np.enterAffiliatedSocietySearchTerms('BMI');
+        np.selectAffiliatedSocietySearchResultByIndex(0, 'BMI');
+
+        np.save();
+    });
+
+    describe('Create work', () => {
+        let base = steps.base;
+
+        base.useBlankEntityDataSlot('work', test.scenario.i);
+
+        // ---
+
+        let nw = steps.newWork;
+
+        nw.goToNewWorkPage();
+
+        nw.enterPrimaryWorkTitle(test.work.title);
+
+        nw.enterCreatorSearchTerms(0, test.scenario.id);
+        nw.selectCreatorSearchResultByIndex(0, test.person.name);
+
+        nw.enterCreatorContribution(0, 100);
+
+        nw.optToIncludeWorkOnWebsite(false);
+    });
+};
+
+exports.feature = [
+    {
+        name: 'Add recording rows and remove them (create mode)',
+
+        tags: [],
+
+        steps: () => {
+            let nw = steps.newWork;
+
+            nw.continueToNextTab();
+
+            // ---
+
+            let wr = steps.workRecordings;
+
+            let testRecRows = [];
+
+            describe('Add some rows', () => {
+                _.times(4, i => {
+                    describe(`Add row ${i}`, () => {
+                        let recRow = {};
+
+                        testRecRows.push(recRow);
+
+                        recRow.id = randomId(
+                            `recording${test.scenario.i}:${i}`
+                        );
+
+                        recRow.title = `TEST RECORDING ${recRow.id}`;
+                        recRow.artistName = `TEST ARTIST ${recRow.id}`;
+
+                        // ---
+
+                        wr.enterTitle(i, recRow.title);
+                        wr.validateEnteredTitle(i, recRow.title);
+
+                        wr.enterArtistSearchTerms(i, recRow.artistName);
+                        wr.createEnteredArtist();
+
+                        wr.validateEnteredArtistSearchTerms(
+                            i, recRow.artistName
+                        );
+
+                        wr.enterDuration(i, stringRepeat(
+                            3, leftPad(i, 2, 0)
+                        ));
+
+                        wr.validateEnteredDuration(i, arrayRepeat(
+                            3, leftPad(i, 2, 0)
+                        ).join(' : '));
+
+                        wr.toggleFirstUseCheckbox(i);
+                        wr.validateFirstUseCheckboxState(i, true);
+
+                        wr.validateRemoveButtonTooltip(i, 'Delete Recording');
+                    });
+                });
+            });
+
+            _.times(2, i => {
+                describe(`Remove row ${i}`, () => {
+                    wr.remove(i);
+                    testRecRows.splice(i, 1);
+                });
+            });
+
+            describe('Validate remaining ones', () => {
+                wr.validateRowCount(testRecRows.length + 1);
+
+                _.times(2, i => {
+                    describe(`Validate row ${i}`, () => {
+                        let recRow = testRecRows[i];
+
+                        wr.validateEnteredTitle(i, recRow.title);
+
+                        wr.validateEnteredArtistSearchTerms(i, recRow.artistName);
+
+                        wr.validateEnteredDuration(i, arrayRepeat(3, leftPad(
+                            i * 2 + 1, 2, 0
+                        )).join(' : '));
+
+                        wr.validateFirstUseCheckboxState(i, i === 1);
+                    });
+                });
+            });
+        }
+    },
+
+    {
+        name: 'Add recording rows and remove them (edit mode)',
+
+        tags: [],
+
+        steps: () => {
+            let nw = steps.newWork;
+
+            nw.save();
+
+            // ---
+
+            let w = steps.work;
+
+            w.goToRecordingsTab();
+
+            // ---
+
+            let wr = steps.workRecordings;
+
+            wr.addRecordings();
+
+            // ---
+
+            let testRecRows = [];
+
+            describe('Add some rows', () => {
+                _.times(4, i => {
+                    describe(`Add row ${i}`, () => {
+                        let recRow = {};
+
+                        testRecRows.push(recRow);
+
+                        recRow.id = randomId(`recording${i}`);
+                        recRow.title = `TEST RECORDING ${recRow.id}`;
+                        recRow.artistName = `TEST ARTIST ${recRow.id}`;
+
+                        // ---
+
+                        wr.enterTitle(i, recRow.title);
+                        wr.validateEnteredTitle(i, recRow.title);
+
+                        wr.enterArtistSearchTerms(i, recRow.artistName);
+                        wr.createEnteredArtist();
+
+                        wr.validateEnteredArtistSearchTerms(
+                            i, recRow.artistName
+                        );
+
+                        wr.enterDuration(i, stringRepeat(
+                            3, leftPad(i, 2, 0)
+                        ));
+
+                        wr.validateEnteredDuration(i, arrayRepeat(
+                            3, leftPad(i, 2, 0)
+                        ).join(' : '));
+
+                        wr.toggleFirstUseCheckbox(i);
+                        wr.validateFirstUseCheckboxState(i, true);
+
+                        wr.validateRemoveButtonTooltip(i, 'Delete Recording');
+                    });
+                });
+            });
+
+            _.times(2, i => {
+                describe(`Remove row ${i}`, () => {
+                    wr.remove(i);
+                    testRecRows.splice(i, 1);
+                });
+            });
+
+            describe('Validate remaining ones', () => {
+                wr.validateRowCount(testRecRows.length + 1);
+
+                _.times(2, i => {
+                    describe(`Validate row ${i}`, () => {
+                        let recRow = testRecRows[i];
+
+                        wr.validateEnteredTitle(i, recRow.title);
+
+                        wr.validateEnteredArtistSearchTerms(i, recRow.artistName);
+
+                        wr.validateEnteredDuration(i, arrayRepeat(3, leftPad(
+                            i * 2 + 1, 2, 0
+                        )).join(' : '));
+
+                        wr.validateFirstUseCheckboxState(i, i === 1);
+                    });
+                });
+            });
+        }
+    },
+
+    {
+        name: 'Save recording rows, then edit and remove them',
+
+        tags: [],
+
+        steps: () => {
+            let nw = steps.newWork;
+
+            nw.continueToNextTab();
+
+            // ---
+
+            let wr = steps.workRecordings;
+
+            let testRecRows = [];
+
+            describe('Add some rows', () => {
+                _.times(4, i => {
+                    describe(`Add row ${i}`, () => {
+                        let recRow = {};
+
+                        testRecRows.push(recRow);
+
+                        recRow.id = randomId(
+                            `recording${test.scenario.i}:${i}`
+                        );
+
+                        recRow.title = `TEST RECORDING ${recRow.id}`;
+                        recRow.artistName = `TEST ARTIST ${recRow.id}`;
+
+                        // ---
+
+                        wr.enterTitle(i, recRow.title);
+                        wr.validateEnteredTitle(i, recRow.title);
+
+                        wr.enterArtistSearchTerms(i, recRow.artistName);
+                        wr.createEnteredArtist();
+
+                        wr.validateEnteredArtistSearchTerms(
+                            i, recRow.artistName
+                        );
+
+                        wr.enterDuration(i, stringRepeat(
+                            3, leftPad(i, 2, 0)
+                        ));
+
+                        wr.validateEnteredDuration(i, arrayRepeat(
+                            3, leftPad(i, 2, 0)
+                        ).join(' : '));
+
+                        wr.toggleFirstUseCheckbox(i);
+                        wr.validateFirstUseCheckboxState(i, true);
+                    });
+                });
+            });
+
+            nw.save();
+
+            // ---
+
+            let w = steps.work;
+
+            w.goToRecordingsTab();
+
+            wr.edit();
+
+            // ---
+
+            let alb = wr.albums;
+
+            _.times(2, i => {
+                describe(`Remove rows ${i}`, () => {
+                    let recRow = testRecRows[i];
+
+                    wr.findByEnteredTitle(recRow.title, 'row index');
+
+                    let iFound = fromTestVariable('row index');
+
+                    alb.toggleExpand(iFound);
+                    alb.toggleExpand(iFound);
+
+                    wr.remove(iFound);
+                    testRecRows.splice(i, 1);
+                });
+            });
+
+            wr.save();
+
+            // ---
+
+            let validateRemainingOnes = () => {
+                describe('Validate remaining ones', () => {
+                    wr.validateRowCount(testRecRows.length + 1);
+
+                    _.times(2, i => {
+                        describe(`Validate row ${i}`, () => {
+                            let recRow = testRecRows[i];
+
+                            wr.findByEnteredTitle(recRow.title, 'row index');
+
+                            let iFound = fromTestVariable('row index');
+
+                            wr.validateEnteredArtistSearchTerms(
+                                iFound, recRow.artistName
+                            );
+
+                            wr.validateEnteredDuration(iFound, arrayRepeat(
+                                3, leftPad(i * 2 + 1, 2, 0)
+                            ).join(' : '));
+
+                            wr.validateFirstUseCheckboxState(iFound, i === 1);
+                        });
+                    });
+                });
+            };
+
+            wr.edit();
+
+            validateRemainingOnes();
+
+            steps.base.refreshPage();
+
+            wr.edit();
+
+            validateRemainingOnes();
+        }
+    }
+];
