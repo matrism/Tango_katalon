bd1ecbda3cd5ae7b24cdb6b1b9eef900631a156e
Guilherme Vieira <guilherme.vieira@wmg.com>
2016-05-30 19:13:35 -0300

    TAT-842: Implement pages.uploadWorks.

---
+++ envs/common/pages/works/upload.js
@@ -0,0 +1,81 @@
+'use strict';
+
+pages.uploadWorks = exports;
+
+exports.fileInput = () => $('[name="work_upload_file"] [type="file"]');
+
+exports.enterFilePath = path => asAlways(
+    exports.fileInput(), 'scrollIntoView', 'clear'
+).sendKeys(path);
+
+exports.sourceTypeahead = () => Typeahead(by.model('dataHolder.model.source'));
+
+exports.enterSourceSearchTerms = val => asAlways(
+    exports.sourceTypeahead(), 'scrollIntoView', 'clear'
+).sendKeys(val);
+
+exports.waitForSourceSearchResults = () => browser.wait(EC.visibilityOfAny(
+    exports.sourceTypeahead().results()
+));
+
+exports.selectSourceSearchResultByIndex = (i, name) => {
+    let result = exports.sourceTypeahead().results().get(i);
+
+    return pages.base.scrollIntoView(result).then(() => {
+        if(name) {
+            expect(pph.getAllText(
+                result.$('.tg-typeahead__item-left')
+            )).toBe(name);
+        }
+    }).then(() => result.click());
+};
+
+exports.formatDropdown = () => element(by.model('dataHolder.model.format'));
+
+exports.openFormatDropdownButton = () => exports.formatDropdown().$(
+    '.tg-dropdown-caret'
+);
+
+exports.formatDropdownOption = name => exports.formatDropdown().element(
+    by.cssContainingText('.tg-dropdown-menu-item', name)
+);
+
+exports.selectFormat = name => {
+    asAlways(
+        exports.openFormatDropdownButton(), 'scrollIntoView', 'click'
+    );
+
+    return asAlways(
+        exports.formatDropdownOption(name), 'scrollIntoView'
+    ).click();
+};
+
+exports.musicLibraryDropdown = () => Typeahead(by.model(
+    'dataHolder.model.musicLibrary'
+));
+
+exports.openMusicLibraryDropdownButton = () => (
+    exports.musicLibraryDropdown().$('.tg-dropdown-caret')
+);
+
+exports.musicLibraryDropdownOption = name => (
+    exports.musicLibraryDropdown().element(by.cssContainingText(
+        '.tg-dropdown-menu-item', name
+    ))
+);
+
+exports.selectMusicLibrary = name => {
+    asAlways(
+        exports.openMusicLibraryDropdownButton(), 'scrollIntoView', 'click'
+    );
+
+    return asAlways(
+        exports.musicLibraryDropdownOption(name), 'scrollIntoView'
+    ).click();
+};
+
+exports.uploadButton = () => $('[data-ng-click="uploadFile();"]');
+
+exports.upload = () => asAlways(
+    exports.uploadButton(), 'scrollIntoView', 'click'
+);
