908945616931222cb392c1881bac6f3432b74508
denisafericean <denisafericean>
2016-06-03 15:06:24 +0300

    TAT-76

--- envs/common/features/deals/createViewEditScopes.js
+++ envs/common/features/deals/createViewEditScopes.js
@@ -185,7 +185,7 @@ exports.feature = [
             steps.createDealScope.fillIntoPublisherNameAMCollectFieldChainI(2);
             //add third chain for pss
             steps.createDealScope.clickAddChainLink();
-            steps.createDealScope.fillPublisherNameFieldsBasedOnPublisherTypeEOrPAChainIValuePercentCollect(2, "name", "34", "24");
+            steps.createDealScope.fillPublisherNameFieldsBasedOnPublisherTypeEOrPAChainIValuePercentCollect(3, "name", "34", "24");
             steps.createDealScope.fillIntoPublisherNameAMFieldChainI(3);
             steps.createDealScope.selectSpecificPublisherNameDropDownChainI(3);
             steps.createDealScope.fillIntoPublisherNameAMCollectFieldChainI(3);
