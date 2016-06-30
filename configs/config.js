54a5badce1cc7a37914a2a3d3ff965db9dc29e68
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2016-06-30 16:28:54 -0300

    Overwrite user env var

--- configs/config.js
+++ configs/config.js
@@ -40,8 +40,10 @@ if (userConfig.cli) {
     console.log(`User configuration params: ${Object.keys(userConfig.cli)}`);
 }
 
-var defaultUserName = cli['user'] || 'TangoTest1',
-    defaultPassword = cli['password'] || 'P@ssw0rd78',
+var defaultUserName = 'TangoTest1',
+    defaultPassword = 'P@ssw0rd78',
+    user = cli['user'] || configer.getEnvVarByKey('TEST_USERNAME') || defaultUserName,
+    password = cli['password'] || configer.getEnvVarByKey('TEST_PASSWORD') || defaultPassword,
     config = {
         _default_: {
             client_id: 'devportal',
@@ -108,8 +110,8 @@ var defaultUserName = cli['user'] || 'TangoTest1',
                     cli['royalties-url'] || 'http://tanroysrv.tango.qa.wmg.com:80'
                 )
             },
-            user_name: configer.getEnvVarByKey('TEST_USERNAME') || defaultUserName,
-            user_password: configer.getEnvVarByKey('TEST_PASSWORD') || defaultPassword
+            user_name: user,
+            user_password: password
         },
         staging: {
             urls: {
@@ -120,8 +122,8 @@ var defaultUserName = cli['user'] || 'TangoTest1',
                     'http://tancrsrv.internal.staging.wmg.com:80'
                 )
             },
-            user_name: configer.getEnvVarByKey('TEST_USERNAME') || defaultUserName,
-            user_password: configer.getEnvVarByKey('TEST_PASSWORD') || defaultPassword
+            user_name: user,
+            user_password: password
         },
         prod: {
             urls: {
@@ -129,8 +131,8 @@ var defaultUserName = cli['user'] || 'TangoTest1',
                 app_url: cli['app-url'],
                 cr_url: cli['cr-url']
             },
-            user_name: configer.getEnvVarByKey('TEST_USERNAME') || defaultUserName,
-            user_password: configer.getEnvVarByKey('TEST_PASSWORD') || defaultPassword
+            user_name: user,
+            user_password: password
         }
     };
 
