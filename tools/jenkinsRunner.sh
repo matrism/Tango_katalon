f88ff72e985321be54a5ee12057fb7925fbbfab7
Daniel Arenhart <Daniel.Arenhart@wmg.com>
2016-07-20 12:30:15 -0300

    development-fix-selenium-grid-param: fix checkbox param

--- tools/jenkinsRunner.sh
+++ tools/jenkinsRunner.sh
@@ -54,8 +54,7 @@ if [ -n "$cycle" ]; then
     cmd_line+=" --cycle $(printf '%q' "$cycle") "
 fi
 
-echo 'DEBUG' $use_selenium_server
-if [ -n "$use_selenium_server" ]; then
+if [ "$use_selenium_server" != 'false' ]; then
     cmd_line+=" --use-selenium-server "
 fi
 
