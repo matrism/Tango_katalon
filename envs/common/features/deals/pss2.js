adc86bed61544b3e5e60d0614350281484a44be1
denisafericean <denisafericean>
2016-07-22 14:34:00 +0300

    TAT-674

--- envs/common/features/deals/pss2.js
+++ envs/common/features/deals/pss2.js
@@ -15,15 +15,15 @@ exports.feature = [
         steps: function () {
             steps.createDealGeneral.itFillDealMandatoryFieldsGeneralTab();
             steps.deal.itContinueToNextPage();
-            //steps.createDealContractPeriod.itFillDealMandatoryFieldsContractPeriod();
-            //steps.createDealScope.itAddSimpleScope();
-            //steps.createDealScope.itAddPublisherShare();
-            //steps.base.scrollIntoView("Save publisher share set ", pages.createDealScope.elems.savePublisherShareSet);
-            //steps.createDealScope.saveThePublisherShareSet();
-            //steps.deal.itContinueToNextPage();
-            //steps.deal.saveDeal();
-            //steps.deal.waitForDealToBeSaved();
-            //steps.deal.returnDealNumber();
+            steps.createDealContractPeriod.itFillDealMandatoryFieldsContractPeriod();
+            steps.createDealScope.itAddSimpleScope();
+            steps.createDealScope.itAddPublisherShare();
+            steps.base.scrollIntoView("Save publisher share set ", pages.createDealScope.elems.savePublisherShareSet);
+            steps.createDealScope.saveThePublisherShareSet();
+            steps.deal.itContinueToNextPage();
+            steps.deal.saveDeal();
+            steps.deal.waitForDealToBeSaved();
+            steps.deal.returnDealNumber();
         }
     }
 ];
