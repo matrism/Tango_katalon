6b90c5562232ffb5b392f8721cc0399c6a5e1dcf
ShahrulIbrahim <shahrul.ibrahim@warnerchappell.com>
2017-05-31 13:01:19 +0800

    ts-346

--- envs/common/pages/royalties/royaltyRates.js
+++ envs/common/pages/royalties/royaltyRates.js
@@ -287,7 +287,7 @@ if (pages.royaltyRates === undefined) {
         },
 
         workInput: function () {
-            return $('div[data-ng-model="workEntries.newWork"] input.tg-typeahead__input.ng-pristine.ng-valid[placeholder="Search by Title, Creator or Work ID"]');
+            return $('[data-ng-model="activeBatch.workEntries.newWork"] input');
         },
 
         amountRecievedInput: function () {
@@ -1066,9 +1066,8 @@ if (pages.royaltyRates === undefined) {
 
             if (sentKeys != "") {
                 var suggestion = $(".tg-typeahead__item-left>strong");
-                //browser.wait(ExpectedConditions.invisibilityOf(suggestion));
                 workInput.sendKeys(sentKeys);
-                suggestion = $(".tg-typeahead__item-left>strong");
+                //browser.wait(ExpectedConditions.invisibilityOf(suggestion));
                 browser.wait(ExpectedConditions.visibilityOf(suggestion));
                 suggestion.click();
             }
