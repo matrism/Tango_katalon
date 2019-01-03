9bc691fc684850f10898ea78f0d1c9da6e60e2df
afinaAshley <afina.ashley@warnerchappell.com>
2019-01-03 12:12:52 +0800

    Search_Person

---
+++ Object Repository/Main Nav Bar/Search_list_dropdown.rs
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Search_list_dropdown</name>
+   <tag></tag>
+   <elementGuidId>fd9dd87c-5ce1-49b3-8f78-09ad2edd921a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='SUB-VIEWS']/div[2]/ui-view/tg-sticky-headers/div[2]/form/div/div/div/div[2]/div/div/div[4]/div/div/div/div/div[2]/ul/li/div/ul/li</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//li[@class = 'tg-typeahead__suggestions-group-item ng-scope' and (contains(text(), '${Person}') or contains(., '${Person}'))]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>li</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>tg-typeahead__suggestions-group-item ng-scope</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-repeat</name>
+      <type>Main</type>
+      <value>$match in $dataSet.queried.matches | limitTo:$dataSet.queried.limit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-class</name>
+      <type>Main</type>
+      <value>{ active: $index === $dataSet.queried.activeIndex, selected: $match.selected, disabled: $match.disabled }</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>${Person}</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;SUB-VIEWS&quot;)/div[@class=&quot;ng-scope&quot;]/ui-view[@class=&quot;ng-scope&quot;]/tg-sticky-headers[@class=&quot;ng-scope ng-isolate-scope&quot;]/div[@class=&quot;create-deal-wizard ng-scope&quot;]/form[@class=&quot;form-horizontal ng-valid-year-integer ng-valid-year-length ng-valid-month-integer ng-valid-month-range ng-valid-day-integer ng-valid-day-range ng-valid-date ng-valid-min ng-valid-future ng-valid-maxlength ng-valid-decimal ng-valid-negative ng-valid-number ng-valid-limit ng-invalid ng-invalid-tags-required ng-dirty ng-valid-parse ng-invalid-required&quot;]/div[@class=&quot;ng-scope&quot;]/div[@class=&quot;span12 nomargins create-deal general-step ng-scope&quot;]/div[@class=&quot;EDITOR span6 editor-left modular-edit ng-scope create ng-valid-year-integer ng-valid-year-length ng-valid-month-integer ng-valid-month-range ng-valid-day-integer ng-valid-day-range ng-valid-date ng-valid-min ng-valid-future ng-valid-maxlength ng-invalid ng-invalid-tags-required ng-dirty ng-valid-parse ng-invalid-required&quot;]/div[@class=&quot;FORM ng-scope&quot;]/div[@class=&quot;form-horizontal ng-scope&quot;]/div[@class=&quot;modular-container clearfix&quot;]/div[@class=&quot;control-group ng-scope error&quot;]/div[@class=&quot;span4 controls&quot;]/div[@class=&quot;deal__company-code ng-untouched ng-isolate-scope ng-dirty ng-invalid ng-invalid-required&quot;]/div[@class=&quot;ng-scope&quot;]/div[@class=&quot;tg-typeahead ng-scope&quot;]/div[@class=&quot;tg-typeahead__suggestions-wrap&quot;]/ul[@class=&quot;tg-typeahead__suggestions ng-scope&quot;]/li[@class=&quot;tg-typeahead__suggestions-container&quot;]/div[@class=&quot;ng-scope&quot;]/ul[@class=&quot;tg-typeahead__suggestions-group&quot;]/li[@class=&quot;tg-typeahead__suggestions-group-item ng-scope active&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//div[@id='SUB-VIEWS']/div[2]/ui-view/tg-sticky-headers/div[2]/form/div/div/div/div[2]/div/div/div[4]/div/div/div/div/div[2]/ul/li/div/ul/li</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Company Name'])[1]/following::li[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Fiscal Entity'])[1]/following::li[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//li/div/ul/li</value>
+   </webElementXpaths>
+</WebElementEntity>
